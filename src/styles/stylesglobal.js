5434dd5db33b8e16c2fd3cead618f98e5ea85842
katyalmeida <almeidakatiane29@gmail.com>
2024-09-11 20:58:02 -0300

    style: global styling

---
+++ src/styles/stylesglobal.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+const globalStyle = createGlobalStyle`
+
+* {
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "comfortaa", sans-serif;
+    font-optical-sizing: auto;
+    
+}
+
+`;
+
+export default globalStyle;
